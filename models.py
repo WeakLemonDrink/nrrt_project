ed6f357ab05213f8ad2cedccfb4239f7b634d5d5
Jonathan Adcock <jonny.adcock@gmail.com>
2020-10-18 19:53:07 +0100

    further prototyping of db models

--- models.py
+++ models.py
@@ -13,7 +13,7 @@ class DataType(models.Model):
         Defines the return string for an `Attribute` db table entry
         '''
 
-        return self.name        
+        return self.name
 
 
 class Attribute(models.Model):
@@ -51,7 +51,7 @@ class Measure(models.Model):
     unit_of_measurement = models.CharField()
     value_dtype = models.ForeignKey(DataType)
     statistic_type = models.CharField() # This could be limited to a choice if there are limited
-                                        # types, or `ForeignKey` to a db table if you want to 
+                                        # types, or `ForeignKey` to a db table if you want to
                                         # search/filter by statistic_type
     measurement_reference_time = models.CharField()
     measurement_precision = models.CharField()
@@ -71,6 +71,8 @@ class Measure(models.Model):
         return self.measure_name
 
 
+# It may be better to combine `ABMLink` and `InstanceLink` tables into one `Link` table
+# as the data looks common across both
 class Link(models.Model):
     '''
     Defines db table for a `Link`
@@ -91,10 +93,10 @@ class Link(models.Model):
 
     def __str__(self):
         '''
-        Defines the return string for an `Link` db table entry
+        Defines the return string for an `ABMLink` db table entry
         '''
 
-        return self.measure_name
+        return self.relationship
 
 
 class AbstractBaseModel(models.Model):
@@ -106,7 +108,7 @@ class AbstractBaseModel(models.Model):
                               # ABM entries together
     attribute = models.ManyToOneField(Attribute)
     measure = models.ManyToOneField(Measure)
-    link = models.ManyToOneField(Link)
+    link = models.ManyToOneField(ABMLink)
 
     def serialize(self):
         '''
@@ -138,4 +140,92 @@ class AbstractBaseModel(models.Model):
         '''
 
         return self.name
-        
+
+
+# See `ABMLink` above
+class InstanceLink(models.Model):
+    '''
+    Defines db table for `InstanceLink`
+    '''
+
+    relationship = models.CharField()
+    landing_instance = models.CharField() # Could be a `models.UrlField` if this is always a url
+
+    def serialize(self):
+        '''
+        Returns a entry serialized to json format (e.g. when constructing the returned data)
+        '''
+
+        pass
+
+    def __str__(self):
+        '''
+        Defines the return string for an `InstanceLink` db table entry
+        '''
+
+        return self.relationship
+
+
+# Need a bit more information on what a `Link` and a `IncomingInteractionLink` are
+class IncomingInteractionLink(models.Model):
+    '''
+    Defines db table for `IncomingInteractionLink`
+    '''
+
+    relationship = models.CharField()
+    origin_instance = models.CharField() # Could be a `models.UrlField` if this is always a url
+
+    def serialize(self):
+        '''
+        Returns a entry serialized to json format (e.g. when constructing the returned data)
+        '''
+
+        pass
+
+    def __str__(self):
+        '''
+        Defines the return string for an `IncomingInteractionLink` db table entry
+        '''
+
+        return self.relationship
+
+
+class Instance(models.Model):
+    '''
+    Defines db table for `Instance`
+    '''
+
+    abm = models.ForeignKey(AbstractBaseModel)
+    attribute = models.JsonField()
+    measure = models.JsonField()
+    link = models.ManyToOneField(InstanceLink) # e.g. (Book)<-[WROTE]-(Person)
+    iil = models.ManyToOneField(IncomingInteractionLink)
+
+    def serialize(self):
+        '''
+        Returns a entry serialized to json format (e.g. when constructing the returned data),
+        using methods defined in related entries
+        '''
+
+        return_data = {
+            'ABM': str(self.abm),
+            'ATTR': self.attribute,
+            'MEAS': self.measure,
+            'LINK': [],
+            'IIL': []
+        }
+
+        for link in self.link.objects.all():
+            return_data['LINK'].append(link.serialize())
+
+        for iil in self.iil.objects.all():
+            return_data['IIL'].append(iil.serialize())
+
+        return return_data
+
+
+class RankingCluster(models.Model):
+    '''
+    Defines db table for `RankingCluster`
+    '''
+
